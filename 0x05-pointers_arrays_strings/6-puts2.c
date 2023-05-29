5d222a8262cf378b8178c8c57256903f566a61e9
Fabulash1 <wizzlin1993pp@gmial.com>
2023-05-29 06:05:26 -0700

    Tasks for pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+* puts2 - function prints a string, then a new line, to stdout
+* @str: a pointer value
+* Return: Always null
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		 i++;
+	}
+
+	_putchar('\n');
+}
