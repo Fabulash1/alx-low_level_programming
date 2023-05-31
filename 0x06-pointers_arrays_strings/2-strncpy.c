fa3a00ddfb652ad7d9d4f479613a315d7b4ebf5a
Fabulash1 <wizzlin1993pp@gmial.com>
2023-05-31 12:27:52 -0700

    Tasks for more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: Pointer to a character that will be changed
+ * @src: Pointer to a character that will be changed
+ * @n: value
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int p;
+
+	p = 0;
+
+	while (p < n && src[p] != '\0')
+	{
+		dest[p] = src[p];
+		p++;
+	}
+
+	while (p < n)
+	{
+		dest[i] = '\0';
+		p++;
+	}
+
+	return (dest);
+}
