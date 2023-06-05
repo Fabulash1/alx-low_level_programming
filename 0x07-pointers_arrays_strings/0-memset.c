6e03962a6a3fd671bf1fd15b22018d154763aa75
Fabulash1 <wizzlin1993pp@gmial.com>
2023-06-05 12:09:02 -0700

    Task more of pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,13 @@
 /**
  *_memset - fills memory with a constant byte.
  *
- *fills the first n bytes of the memory area pointed
- * 
- *to by s with the constant b.
- *
+ *fills the first n bytes of the memory area pointed to by s with b.
  *@s: pointer to memory area.
  *@b: constant to fill memory with.
  *@n: bytes of the memory area to be filled.
  *Return: to memory area s.
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int counter;
