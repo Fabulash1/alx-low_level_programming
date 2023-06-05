efd1109054a242f557ae07f2772c3a8b10a7fbc2
Fabulash1 <wizzlin1993pp@gmial.com>
2023-06-05 11:53:20 -0700

    Task for more of pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - set value of pointer to a character
+*
+* @s: variable of type pointer
+* @to: character
+*
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
